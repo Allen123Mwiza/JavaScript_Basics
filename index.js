f82b500273085dc2c01198c36e866ee06dbb1073
Allen123Mwiza <116109466+Allen123Mwiza@users.noreply.github.com>
2023-02-13 11:40:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function multiply(number1, number2) {
 
 console.log(multiply(200,12));
 
+let firstname="Allen";
+let lastName="Mwiza";
+let Stuterntrack="frontend developer";
+let skills= "Css,Html, JavaScript and  Python.";
+let favouriteColor= "Purple.";
+let yearofCountryIndependence="1st July 1962";
+
+
+console.log("My name is " + firstname + " " + lastName + ", I am a " + Stuterntrack +  " Who is skilled in " + skills + "My favourite color is " + favouriteColor + " My country got her Independence on " + yearofCountryIndependence + "." )
